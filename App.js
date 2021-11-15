d85d4995113746cea273eb0b7fc0fb3442d4961d
Ada Pauline Villacarlos <miku_marvell@yahoo.com.ph>
2021-11-15 19:50:58 +0800

    lists & scroll view

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
   const[people, setPeople] = useState([
@@ -8,12 +8,20 @@ export default function App() {
     {name: 'Romel', key: '2'}, 
     {name: 'Ronerr', key: '3'},
     {name: 'Nerressa', key: '4'}, 
-    {name: 'Romeo', key: '5'}
+    {name: 'Romeo', key: '5'}, 
+    {name: 'Sarah', key: '6'}, 
+    {name: 'Adalona', key: '7'}, 
   ]); 
 
   return (
     <View style={styles.container}>
-      
+      <ScrollView> 
+      { people.map(item => (
+          <View key={item.key}> 
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,8 +31,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40, 
+    paddingHorizontal: 20, 
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   header: {
     backgroundColor: 'black',
@@ -48,7 +58,12 @@ const styles = StyleSheet.create({
     padding: 8, 
     margin: 10, 
     width: 200, 
-
+  }, 
+  item: {
+    marginTop: 24, 
+    padding: 30, 
+    backgroundColor: 'pink', 
+    fontSize: 24, 
   }
 
 });
