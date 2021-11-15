618c5c3ddf10828b609a1e8875da783b5eb9f87d
Ada Pauline Villacarlos <miku_marvell@yahoo.com.ph>
2021-11-15 19:36:23 +0800

    lists and scrollview

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const[people, setPeople] = useState([
+    {name: 'Ada', key:'1'},
+    {name: 'Romel', key: '2'}, 
+    {name: 'Ronerr', key: '3'},
+    {name: 'Nerressa', key: '4'}, 
+    {name: 'Romeo', key: '5'}
+  ]); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +26,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'black',
+    padding: 20, 
+  }, 
+  boldText: {
+    fontWeight: 'bold', 
+    color: 'white', 
+  }, 
+  body: {
+    backgroundColor: 'yellow',
+    margin: 5, 
+    padding: 20, 
+  },
+  buttonContainer: {
+    marginTop: 20,
+  }, 
+  input: {
+    borderWidth: 1, 
+    borderColor: '#777', 
+    padding: 8, 
+    margin: 10, 
+    width: 200, 
+
+  }
+
 });
