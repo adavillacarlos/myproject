a1436d606dd278db2292a7acd0823817b8c58a84
Ada Pauline Villacarlos <miku_marvell@yahoo.com.ph>
2021-11-15 20:07:35 +0800

    touchable components

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const[people, setPeople] = useState([
@@ -13,6 +13,13 @@ export default function App() {
     {name: 'Adalona', id: '7'}, 
   ]); 
 
+  const pressHandler = (id) => {
+    console.log(id); 
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id)
+    }); 
+  }
+
   return (
     <View style={styles.container}>
       <FlatList 
@@ -20,7 +27,9 @@ export default function App() {
         keyExtractor={( item ) => item.id}
         data={people} 
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       
       /> 
